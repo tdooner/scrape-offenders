d6381fe8b9525dd7d8024c6d0cc1d9469ef92498
Tom Dooner <tom.dooner@brigade.com>
2017-04-07 13:08:06 -0700

    Catch 500 error and wait 5 seconds

--- status_check.rb
+++ status_check.rb
@@ -5,7 +5,10 @@
 require 'mechanize'
 require 'json'
 
-def throttle
+def throttle(failed = false)
+  if failed
+    sleep 5
+  end
   sleep 0.1
 end
 
@@ -13,31 +16,38 @@ $stdout.sync = false
 
 scraper = Mechanize.new
 
-scraper.get('http://docpub.state.or.us/OOS/intro.jsf') do |page|
-  search_page = scraper.click 'I Agree'
+begin
+  scraper.get('http://docpub.state.or.us/OOS/intro.jsf') do |page|
+    search_page = scraper.click 'I Agree'
 
-  # ['10042337'].each do |sid|
-  $stdin.each_line do |sid|
-    results_page = search_page.form_with(id: 'mainBodyForm') do |f|
-      f['mainBodyForm:SidNumber'] = sid.strip
-    end.click_button
+    # ['10042337'].each do |sid|
+    $stdin.each_line do |sid|
+      results_page = search_page.form_with(id: 'mainBodyForm') do |f|
+        f['mainBodyForm:SidNumber'] = sid.strip
+      end.click_button
 
-    offenses = results_page.css('[id="offensesForm:offensesTable"] tbody tr:nth-child(2n+1)')
+      offenses = results_page.css('[id="offensesForm:offensesTable"] tbody tr:nth-child(2n+1)')
 
-    $stdout.puts(JSON.generate(
-      sid: results_page.css('[id="offensesForm:out_SID"]').text,
-      age: results_page.css('[id="offensesForm:age"]').text,
-      gender: results_page.css('[id="offensesForm:sex"]').text,
-      # todo all the other fields
-      caseload_number: results_page.css('[id="offensesForm:caseloadNumber"]').text,
-      caseload_name: results_page.css('[id="offensesForm:caseloadMgrsTable"]').text.strip,
-      status: results_page.css('[id="offensesForm:status"]').text,
-      location: results_page.css('[id="offensesForm:locationBlock"] a').text,
-      admission_date: results_page.css('[id="offensesForm:admitDate"]').text,
-      earliest_release_date: results_page.css('[id="offensesForm:relDate"]').text,
-      offenses: offenses.map {|o| o.text.strip.gsub(/\s+/, ',') },
-      num_offenses: offenses.length,
-    ))
+      $stdout.puts(JSON.generate(
+        sid: results_page.css('[id="offensesForm:out_SID"]').text,
+        age: results_page.css('[id="offensesForm:age"]').text,
+        gender: results_page.css('[id="offensesForm:sex"]').text,
+        # todo all the other fields
+        caseload_number: results_page.css('[id="offensesForm:caseloadNumber"]').text,
+        caseload_name: results_page.css('[id="offensesForm:caseloadMgrsTable"]').text.strip,
+        status: results_page.css('[id="offensesForm:status"]').text,
+        location: results_page.css('[id="offensesForm:locationBlock"] a').text,
+        admission_date: results_page.css('[id="offensesForm:admitDate"]').text,
+        earliest_release_date: results_page.css('[id="offensesForm:relDate"]').text,
+        offenses: offenses.map {|o| o.text.strip.gsub(/\s+/, ',') },
+        num_offenses: offenses.length,
+      ))
+
+      throttle
+    end
   end
+rescue Mechanize::ResponseCodeError => ex
+  $stderr.puts "Got Mechanize Error: #{ex.message}"
+  throttle(true)
+  retry
 end
-
